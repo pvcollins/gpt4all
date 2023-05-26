7dac00e305e5e6eaa786e0a0c791c2f93322508e
Peter Collins <16456526+pvcollins@users.noreply.github.com>
2023-05-25 21:10:05 -0400

    Commit to check the other email address

--- my_langchain.py
+++ my_langchain.py
@@ -1,7 +1,9 @@
 # Import of langchain Prompt Template and Chain
 from langchain import PromptTemplate, LLMChain
+
 # Import llm to be able to interact with GPT4All directly from langchain
 from langchain.llms import GPT4All
+
 # Callbacks manager is required for the response handling
 from langchain.callbacks.base import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
